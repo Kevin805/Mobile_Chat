d5f43306c604d0780c340d1166935ec6fb412d09
Kevin Johnson <kevin.michael805@gmail.com>
2021-05-05 11:14:15 -0600

    components started.

--- mobile-chat/src/App.js
+++ mobile-chat/src/App.js
@@ -1,9 +1,17 @@
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { auth } from "./config";
 import './App.css';
+import Login from ".components/Login";
+import ChatRoom from ".components/ChatRoom";
 
 function App() {
+
+  const user = useAuthState(auth);
+
   return (
     <div className="App">
-      app component
+      <div>
+        {user ? <ChatRoom /> : <Login />}</div>
     </div>
   );
 }
