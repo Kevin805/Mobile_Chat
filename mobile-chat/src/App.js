b898d40c443e88fdb79eb9fd39962cbdf89868dd
Kevin Johnson <kevin.michael805@gmail.com>
2021-05-07 13:25:58 -0600

    Chatroom.js working.

--- mobile-chat/src/App.js
+++ mobile-chat/src/App.js
@@ -1,17 +1,18 @@
 import { useAuthState } from 'react-firebase-hooks/auth';
+import { useState } from 'react';
 import Login from "./components/Login";
 import ChatRoom from "./components/ChatRoom";
 import { auth } from "./config";
 import './App.css';
 
 function App() {
-
+  const [currentRoom, setCurrentRoom] = useState("General");
   const [user] = useAuthState(auth);
 
   return (
     <div className="App">
       <div>
-        {user ? <ChatRoom /> : <Login />}</div>
+        {user ? <ChatRoom currentRoom={currentRoom} /> : <Login />}</div>
     </div>
   );
 }
--- mobile-chat/src/components/ChatRoom.js
+++ mobile-chat/src/components/ChatRoom.js
@@ -1,5 +1,35 @@
-const ChatRoom = () => {
-    return <div> Chatroom</div>
+import { useState } from "react";
+import { db, auth, firebaseRef } from "../config";
+
+const ChatRoom = ({ currentRoom }) => {
+    const [message, setMessage] = useState("");
+
+    const messagesRef = db.collection("messages");
+
+    const handleSubmit = async () => {
+        const { uid, photoURL } = auth.currentUser;
+        const createdAt = firebaseRef.firestore.FieldValue.serverTimestamp();
+            await messagesRef.add({
+                uid,
+                photoURL,
+                createdAt,
+                text: message,
+                room: currentRoom,
+            });
+    }
+
+    return (
+        <div className="messages">
+            <form onSubmit={handleSubmit}>
+             <textarea
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
+                placeholder="Enter message"
+            />
+                <button type="submit" disabled={!message}>Send</button>
+            </form>
+        </div>
+    );
 };
 
 export default ChatRoom
