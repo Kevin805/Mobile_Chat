15d45837fd6afbeaf0d03ed013cff0e07ce9b4f5
Kevin Johnson <kevin.michael805@gmail.com>
2021-05-12 12:07:24 -0600

    NavBar work.

--- mobile-chat/src/components/NavBar.js
+++ mobile-chat/src/components/NavBar.js
@@ -6,7 +6,24 @@ const NavBar = ({ user, currentRoom, setCurrentRoom }) => {
     const [showSideBar, setShowSideBar] = useState(false);
 
     return (
-       
+        <nav>
+            <h1>
+                Current room: <strong>{currentRoom}</strong>
+            </h1>
+            {user ? (
+                <>
+                 <button
+                    className="menu"
+                    onClick={() => {
+                        setShowSideBar(!showSideBar);   
+                    }}
+                 >
+                    
+                    
+                 </button>
+            )}
+        
+        </nav>
 
 
 
