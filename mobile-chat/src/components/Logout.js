6b5a75d510ccc1098bb04bbd41ff8b5cbb6f6669
Kevin Johnson <kevin.michael805@gmail.com>
2021-05-12 11:56:53 -0600

    signout started.

---
+++ mobile-chat/src/components/Logout.js
@@ -0,0 +1,16 @@
+import { auth } from "../config";
+
+const SignOut = ({ setShowSideBar }) =>
+auth.currentUser && (
+    <button
+        className="logout"
+        onClick={() => {
+            auth.signOut();
+            setShowSideBar(false);
+        }}
+    >
+      Signout
+    </button>
+);
+
+export default SignOut
